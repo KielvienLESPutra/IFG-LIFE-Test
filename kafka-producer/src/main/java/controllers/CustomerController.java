da53875a7a9d6846d998cf30defeee8d293e59af
Kielvien L E S Putra <for2university@gmail.com>
2025-11-07 16:41:36 +0700

    feature/kafka-producer: Adding check violation constaraint

--- kafka-producer/src/main/java/controllers/CustomerController.java
+++ kafka-producer/src/main/java/controllers/CustomerController.java
@@ -1,8 +1,12 @@
 package controllers;
 
+import java.util.Set;
+
 import org.jboss.logging.Logger;
 
 import entities.Customer;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -12,15 +16,17 @@ import jakarta.ws.rs.core.Response;
 import models.CustomerCreateRequest;
 import services.CustomerService;
 
-@Path("/ap/v1")
+@Path("/api/v1")
 public class CustomerController {
 	
 	private Logger log;
 	private CustomerService customerService;
+	private Validator validator;
 	
-	public CustomerController(Logger log, CustomerService customerService) {
+	public CustomerController(Logger log, CustomerService customerService, Validator validator) {
 		this.log = log;
 		this.customerService = customerService;
+		this.validator = validator;
 	}
 
 	@POST
@@ -29,7 +35,14 @@ public class CustomerController {
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response createNewCustomer(CustomerCreateRequest request) {
 		log.infof("Incoming request create new customer with payload : %s", request.toString());
+		
 		try {
+			Set<ConstraintViolation<CustomerCreateRequest>> violations = validator.validate(request);
+			if (!violations.isEmpty()) {
+				log.warnf("Validation catch violations with reason : %s", violations);
+				return Response.status(Response.Status.BAD_REQUEST).build();
+			}
+			
 			Customer customer = customerService.doSave(request);
 			return Response.ok(customer).build();
 		}catch (Exception e) {
